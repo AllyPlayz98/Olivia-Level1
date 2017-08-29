b1984af08f5b1d590c19b1334e9864a6779605b3
FruitWave <league@WTS8.attlocal.net>
2017-08-28 17:02:26 -0700

    saw apaara today!

---
+++ src/HeyHeyRunner.java
@@ -0,0 +1,14 @@
+
+public class HeyHeyRunner {
+public static void main(String[] args){
+	HeyHey hey = new HeyHey("julia");
+	hey.swallows(2);
+	hey.trips(" BWAK! *OUCH!*");
+	hey.screams();
+	
+	HeyHey hey2 = new HeyHey("wendy");
+	hey2.trips(" OUCH!");
+	
+	HeyHey hey3 = new HeyHey("apaara");
+}
+}
---
+++ src/catRunner.java
@@ -0,0 +1,18 @@
+
+public class catRunner {
+	public static void main(String[] args) {
+		/* Do the following things without changing the Cat class √ */
+Cat kitty = new Cat("whiskers");
+		// 1. Make the Cat meow √
+		kitty.meow();
+		// 2. Get the Cat to print it's name √
+kitty.printName();
+		// 3. Kill the Cat! √
+for (int l = 0; l <= 8; l++) {
+	kitty.kill();
+}
+Cat kitty2 = new Cat("fluffy");
+
+kitty2.printName();
+	}
+}
