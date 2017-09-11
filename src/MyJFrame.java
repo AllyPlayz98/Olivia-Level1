302f44ffa32189b77927fed1c2e607491c1a0151
ThomasTwomey <league@WTS15.attlocal.net>
2017-09-11 16:57:09 -0700

    First Class with Alex!

---
+++ src/MyJFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class MyJFrame {
+public static void main(String[] args) {
+	
+	JFrame f = new JFrame();
+	f.setVisible(true);
+	f.pack();
+	
+	JPanel p = new JPanel();
+	
+	f.add(p);
+	
+	JButton a = new JButton("Click Me!");
+	
+	p.add(a);
+	
+	f.pack();
+	
+	JButton b = new JButton("Push Me!");
+	
+	p.add(b);
+	
+	f.pack();
+}
+}
