eab1012f782b76e1674d7db6f76368d2149957ae
League <league@WTS8.attlocal.net>
2018-02-26 17:00:09 -0800

    Candyman, TypingTutor, Olympics ip, Athletes ip

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,13 +1,17 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class TypingTutor implements KeyListener {
 	char currentLetter = generateRandomLetter();
 	JLabel jl1 = new JLabel();
+	int score = 0;
 
 	public static void main(String[] args) {
 		TypingTutor tt = new TypingTutor();
@@ -24,6 +28,7 @@ public class TypingTutor implements KeyListener {
 		jl1.setHorizontalAlignment(JLabel.CENTER);
 		jf1.add(jl1);
 		jl1.setText(String.valueOf(currentLetter));
+		jl1.setOpaque(true);
 		jf1.pack();
 	}
 
@@ -41,7 +46,18 @@ public class TypingTutor implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		System.out.println("You Typed: " + e.getKeyChar());
+		if (e.getKeyChar() == currentLetter) {
+			jl1.setBackground(new Color(000, 187, 000));
+			score++;
+			if (score == 20) {
+				System.out.println("Game Over!");
+				showTypingSpeed(20);
+				System.exit(0);
+			}
+		} else {
+			jl1.setBackground(new Color(255, 051, 017));
+		}
 	}
 
 	@Override
@@ -49,6 +65,18 @@ public class TypingTutor implements KeyListener {
 		// TODO Auto-generated method stub
 		currentLetter = generateRandomLetter();
 		jl1.setText(String.valueOf(currentLetter));
+
 	}
+	
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+	      Date timeAtEnd = new Date();
+	      long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+	      long gameInSeconds = (gameDuration / 1000) % 60;
+	      double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+	      int charactersPerMinute = (int) (charactersPerSecond * 60);
+	      JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+	}
+	Date timeAtStart = new Date();
+
 
 }
