9960078fe512e042c2f3c8d2c6b0c38629d875a5
League <league@WTS8.attlocal.net>
2018-03-05 16:57:32 -0800

    Olympics ip, OlympicAthlete ip #2

--- src/OlympicAthlete.java
+++ src/OlympicAthlete.java
@@ -1,10 +1,23 @@
 public class OlympicAthlete {
-	String name;
-	String country;
-	String sport;
-public OlympicAthlete(String name, String country, String sport){
-this.name = name;
-this.country = country;
-this.sport = sport;
-}
+	private String name;
+	private String country;
+	private String sport;
+	private int gold;
+	private int silver;
+	private int bronze;
+
+	public OlympicAthlete(String name, String country, String sport) {
+		this.name = name;
+		this.country = country;
+		this.sport = sport;
+	}
+	void setGold(int gnum) {
+		this.gold = gnum;
+	}
+	void setSilver(int snum ) {
+		this.silver = snum;
+	}
+	void setBronze(int bnum) {
+		this.bronze = bnum;
+	}
 }
--- src/Olympics.java
+++ src/Olympics.java
@@ -1,5 +1,11 @@
 public class Olympics {
 public static void main(String[] args) {
-
+ OlympicAthlete oa1 = new OlympicAthlete("Olivia", "U.S.A", "Sleading");
+ OlympicAthlete oa2 = new OlympicAthlete("Julia", "Canada", "Half-Pipe");
+ OlympicAthlete oa3 = new OlympicAthlete("Wendy", "Netherlands", "Skiing");
+ OlympicAthlete oa4 = new OlympicAthlete("Alex", "Slovakia", "Snowboarding");
+ 
+ oa3.setSilver(1);
+ oa3.setBronze(1);
 }
 }
