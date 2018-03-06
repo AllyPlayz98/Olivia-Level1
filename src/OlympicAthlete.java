5f95dadb326b326152fd9fad430d22dbabacb36a
League <league@WTS8.attlocal.net>
2018-03-05 16:56:16 -0800

    Olympics ip, Olympic Athete ip,

--- src/OlympicAthlete.java
+++ src/OlympicAthlete.java
@@ -1,4 +1,10 @@
-
 public class OlympicAthlete {
-
+	String name;
+	String country;
+	String sport;
+public OlympicAthlete(String name, String country, String sport){
+this.name = name;
+this.country = country;
+this.sport = sport;
+}
 }
--- src/Olympics.java
+++ src/Olympics.java
@@ -1,4 +1,5 @@
-
 public class Olympics {
+public static void main(String[] args) {
 
+}
 }
