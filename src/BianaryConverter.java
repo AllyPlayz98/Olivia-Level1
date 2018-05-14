dbb27a5dfde241cd7e2d67a0dcf44b89ee7c7c38
AllyPlayz98 <league@WTS19.attlocal.net>
2018-05-14 16:54:16 -0700

    BianaryConverter ip

---
+++ src/BianaryConverter.java
@@ -0,0 +1,28 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BianaryConverter {
+	JFrame jf1 = new JFrame();
+	JPanel jp1 = new JPanel();
+	JTextField jtf1 = new JTextField(20);
+	JTextField jtf2 = new JTextField(20);
+	JButton jb1 = new JButton();
+	public static void main(String[] args) {
+	//You’ll need a JFrame and a JPanel. √
+	//On the panel, put two text fields and a button. A text field is made like this:
+jf1.add(jp1);
+jp1.add(jtf1);
+jp1.add(jb1);
+jp1.add(jtf2);
+jp1.setVisible(true);
+	//It may look like this or you might have a better layout in mind.
+
+
+
+	//Use the method below to do the conversion:
+
+	
+	}
+}
