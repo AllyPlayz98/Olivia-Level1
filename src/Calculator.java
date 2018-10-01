e7b6f58a262531b3b93385c3a3e80afb43bbae44
SunStar56 <league@WTS22.attlocal.net>
2018-10-01 16:55:36 -0700

    Calculator ip

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,13 +3,27 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class Calculator {
-public static void main(String[] args) {
-JFrame jf1 = new JFrame();
-JTextField jtf1 = new JTextField();
-JTextField jtf2 = new JTextField();
-JButton jb1 = new JButton();
-JButton jb2 = new JButton();
-JButton jb3 = new JButton();
-JButton jb4 = new JButton();
-}
+	public static void main(String[] args) {
+		JFrame jf1 = new JFrame();
+		JTextField jtf1 = new JTextField();
+		JTextField jtf2 = new JTextField();
+		JButton jb1 = new JButton();
+		JButton jb2 = new JButton();
+		JButton jb3 = new JButton();
+		JButton jb4 = new JButton();
+		jf1.setVisible(true);
+		jf1.setSize(400, 200);
+		jf1.add(jtf1);
+		jf1.add(jtf2);
+		jf1.add(jb1);
+		jf1.add(jb2);
+		jf1.add(jb3);
+		jf1.add(jb4);
+		jb1.setSize(80, 35);
+		jb2.setSize(80, 35);
+		jb3.setSize(80, 35);
+		jb4.setSize(80, 35);
+		jb1.setLocation(200, 100);
+
+	}
 }
