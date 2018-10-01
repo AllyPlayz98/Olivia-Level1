bb7322bc6982051359265251fba60cfbe6bf6856
AllyPlayz98 <league@WTS-IM-17.attlocal.net>
2018-10-01 15:54:52 -0700

    Mouse Problem, Computer Transfer

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Calculator {
-
+public static void main(String[] args) {
+JFrame jf1 = new JFrame();
+JTextField jtf1 = new JTextField();
+JTextField jtf2 = new JTextField();
+JButton jb1 = new JButton();
+JButton jb2 = new JButton();
+JButton jb3 = new JButton();
+JButton jb4 = new JButton();
+}
 }
